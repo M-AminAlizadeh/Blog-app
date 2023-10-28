8b90326319da041f2d7d8c4c331dba574f5d625f
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-10-28 15:15:23 +0330

    Change plural for has_many associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  has_many :post, foreign_key: 'author_id'
-  has_many :comment
-  has_many :like
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
 
   def display_three_recent_posts
-    Post.where(author: self).order(created_at: :desc).first(3)
+    posts.order('created_at Desc').limit(3)
   end
 end
