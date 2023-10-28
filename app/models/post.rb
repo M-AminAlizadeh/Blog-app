59975f2f3eee7a3814a1e8d4c8e2815a07e84f7b
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-10-28 13:24:03 +0330

    Add method to show last 5 comments of the post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   has_many :comment
   has_many :like
   belongs_to :author, class_name: 'User'
+
+  def display_five_recent_comments
+    Comment.where(post: self).order(created_at: :desc).first(5)
+  end
 end
