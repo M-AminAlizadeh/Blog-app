272fcf7fda105179d35f32c5d99e90affaab07c7
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-11-03 20:24:45 +0330

    Add general layout and declare users and user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  def index; end
+  layout 'general'
 
-  def show; end
+  def index
+    @users = User.order(id: :asc)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
