8da9712dec59b81b60b78fe20ede7e73c1841d39
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-11-08 10:00:59 +0330

    Add seed to the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+user_1 = User.create(name: 'Tom', photo: 'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80', bio: 'Teacher from Mexico.')
+user_2 = User.create(name: 'Lilly', photo: 'https://images.unsplash.com/photo-1664575602554-2087b04935a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80', bio: 'Teacher from Poland.')
+user_3 = User.create(name: 'Jennifer', photo: 'https://images.pexels.com/photos/5212317/pexels-photo-5212317.jpeg?auto=compress&cs=tinysrgb&w=600', bio: 'Software Developer from Berlin.')
+
+post_1 = Post.create(author: user_1, title: 'Hello Rails', text: 'This is my first project!', comments_counter: 0, likes_counter: 0)
+post_2 = Post.create(author: user_2, title: 'Hello World!', text: 'This is the first post of second user author', comments_counter: 0, likes_counter: 0)
+post_3 = Post.create(author: user_3, title: 'Learning Rails', text: 'This is the second post of first_user author', comments_counter: 0, likes_counter: 0)
+post_4 = Post.create(author: user_1, title: 'Learning ORM in Rails', text: 'This is the second post of second_user author', comments_counter: 0, likes_counter: 0)
+post_5 = Post.create(author: user_2, title: 'Working with DSA in Javascript.', text: 'This is the first post of third_user author', comments_counter: 0, likes_counter: 0)
+
+Comment.create(post: post_1, text: 'Hi Tom!')
