bf7e794e38608c86b63d5c421d289b73d8ac497c
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-11-16 11:31:23 +0330

    Clean code the models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   has_many :comments
   has_many :likes
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   after_save :update_posts_counter
 
   validates :title, presence: true, length: { maximum: 250 }
@@ -9,7 +9,7 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def display_five_recent_comments
-    Comment.where(post: self).order(created_at: :desc).first(5)
+    comments.limit(5).order(created_at: :desc)
   end
 
   def update_posts_counter
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def display_three_recent_posts
-    posts.order('created_at Desc').limit(3)
+    posts.order(created_at: :desc).first(3)
   end
 end
