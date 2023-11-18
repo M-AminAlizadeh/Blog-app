d8724c8bde61cb7348600ab15ea44fd823a039d5
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-11-18 04:02:26 +0330

    Prevent name field as a main element in login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  validates :name, presence: true
+  # validates :name, presence: true
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def display_three_recent_posts
